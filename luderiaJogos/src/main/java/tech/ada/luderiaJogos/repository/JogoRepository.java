8ccb624b6fb6344fe75c54e02580fcefcccb32a3
Juliana Barros <julianabarrosf@protonmail.com>
2024-11-08 16:24:25 -0300

    Criação de novas queries para o repositório de Jogos.

--- luderiaJogos/src/main/java/tech/ada/luderiaJogos/repository/JogoRepository.java
+++ luderiaJogos/src/main/java/tech/ada/luderiaJogos/repository/JogoRepository.java
@@ -3,10 +3,14 @@ package tech.ada.luderiaJogos.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tech.ada.luderiaJogos.model.Jogo;
+import tech.ada.luderiaJogos.model.Mecanica;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface JogoRepository extends JpaRepository<Jogo, Long>{
-    public List<Jogo> findByMecanica();
+    public List<Jogo> findByMecanica(Mecanica mecanica);
+    public Optional<Jogo> findByTitulo(String titulo);
+    public List<Jogo> findByTituloContaining(String busca);
 }
