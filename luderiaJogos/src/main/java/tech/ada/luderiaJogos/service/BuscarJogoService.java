d029c7c10548fd445e4aabe7d9535fe006fd474e
Juliana Barros <julianabarrosf@protonmail.com>
2024-11-08 16:40:28 -0300

    Criação do serviço de buscar Jogos.

---
+++ luderiaJogos/src/main/java/tech/ada/luderiaJogos/service/BuscarJogoService.java
@@ -0,0 +1,35 @@
+package tech.ada.luderiaJogos.service;
+
+import org.springframework.stereotype.Service;
+import tech.ada.luderiaJogos.model.Jogo;
+import tech.ada.luderiaJogos.model.Mecanica;
+import tech.ada.luderiaJogos.repository.JogoRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BuscarJogoService {
+
+    private final JogoRepository jogoRepository;
+
+    public BuscarJogoService(JogoRepository jogoRepository){
+        this.jogoRepository = jogoRepository;
+    }
+
+    public Optional<Jogo> buscarJogoPorId(Long id){
+        return jogoRepository.findById(id);
+    }
+
+    public Optional<Jogo> buscarJogoPorTituloExato(String titulo){
+        return jogoRepository.findByTitulo(titulo);
+    }
+
+    public List<Jogo> buscarJogosPorMecanica(Mecanica mecanica){
+        return jogoRepository.findByMecanica(mecanica);
+    }
+
+    public List<Jogo> buscarJogosPorTitulo(String titulo){
+        return jogoRepository.findByTituloContaining(titulo);
+    }
+}
